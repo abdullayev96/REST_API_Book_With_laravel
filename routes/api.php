418d872ce5befecba8df79f740b954d16ba1539f
Burhon Abdullaev <abdullaev_b@turonbank.uz>
2025-06-20 11:03:40 +0500

    yangi model, controller, migration qoshildi

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\CustomerController;         // yangi controller
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -11,3 +13,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/books', [BookController::class, 'index']);
 Route::post('/books', [BookController::class, 'store']);
+
+Route::get('/customers', [CustomerController::class, 'index']);
+Route::get('/customers/{id}', [CustomerController::class, 'show']);
+Route::post('/customers', [CustomerController::class, 'store']);
+
+//Route::put('/customers/{id}', [CustomerController::class, 'update']);
+//Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
